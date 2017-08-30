f97c7101338390196f4c9162cfff2d965c18fefc
kaylamoore <kaylamoo@usc.edu>
2017-08-29 17:51:59 -0700

    fetches bookmarks from local storage onload and displays them on page

--- js/main.js
+++ js/main.js
@@ -7,13 +7,52 @@ function saveBookmark(e){
   var siteName =document.getElementById('siteName').value;
   var siteUrl =document.getElementById('siteUrl').value;
 
-
+//save form values as a bookmark 
   var bookmark = {
     name: siteName,
     url: siteUrl
   }
-  console.log(bookmark);
-
+//local storage 
+	//is bookmark empty ?
+	if(localStorage.getItem('bookmarks') === null){
+		//initializing an empty array for bookmarks
+		var bookmarks = [];
+		//add the bookmark to the empty bookmarks array 
+		bookmarks.push(bookmark);
+		// set to localstorage and turn it into string beforehand rather than json data 
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+	} else {
+		//get bookmarks from localstorage 
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks')); 
+		//add bookmark to the array 
+		bookmarks.push(bookmark);
+		//re-set back to local storage !
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+	}
+//prevent submit form action 
   e.preventDefault();
+}
+
+//receive the bookmarks in order to display on the page 
+// Fetch bookmarks
+function fetchBookmarks(){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
 }
  
