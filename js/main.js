891e419579a0c54740dec7ef62fcdbde48c12556
kaylamoore <kaylamoo@usc.edu>
2017-08-29 17:55:09 -0700

    adds delete function to the bookmarks saved

--- js/main.js
+++ js/main.js
@@ -33,6 +33,26 @@ function saveBookmark(e){
   e.preventDefault();
 }
 
+
+// Delete bookmark
+function deleteBookmark(url){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Loop throught bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+}
+
+
 //receive the bookmarks in order to display on the page 
 // Fetch bookmarks
 function fetchBookmarks(){
